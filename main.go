fff56817dac5294c5d48f34a41529620b558c099
agreen254 <aarongreen1997@gmail.com>
2025-06-18 15:17:00 -0500

    fourth

--- main.go
+++ main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("This is bait to get my github going lol.")
 	fmt.Println("This is some more bait to get my github going lol.")
 	fmt.Println("The third bait.")
+	fmt.Println("The fourth bait.")
 }
